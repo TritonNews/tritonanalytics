36b9ee90f4ec5d43d94d8ad5112b200ba0ae90a8
dmhacker <dmhacker@yahoo.com>
2017-11-27 18:05:04 -0800

    ../current/ working as planned

--- tritonanalytics/views.py
+++ tritonanalytics/views.py
@@ -1,7 +1,10 @@
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
+from jinja2.exceptions import TemplateNotFound
 
 import os
+import glob
+import ntpath
 
 main = Blueprint('main', __name__)
 graphs = Blueprint('graphs', __name__)
@@ -16,8 +19,19 @@ def graphs_landing_page():
 
 @graphs.route('/page/<analytics_id>')
 def graphs_page_analytics(analytics_id):
-  return render_template('graphs/page/{0}.html'.format(analytics_id))
+  return graph_analytics(analytics_id, 'page')
 
 @graphs.route('/posts/<analytics_id>')
-def graphs_post_analytics(analytics_id):
-  return render_template('graphs/posts/{0}.html'.format(analytics_id))
+def graphs_posts_analytics(analytics_id):
+  return graph_analytics(analytics_id, 'posts')
+
+def graph_analytics(analytics_id, analytics_type):
+  if analytics_id == 'current':
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    analytics_path = os.path.join(dir_path, 'templates', 'graphs', analytics_type, '*.html')
+    template_file = max(glob.iglob(analytics_path), key=os.path.getctime)
+    analytics_id = ntpath.basename(template_file).replace('.html', '')
+  try:
+    return render_template('graphs/{0}/{1}.html'.format(analytics_type, analytics_id))
+  except TemplateNotFound:
+    return abort(404)
