cf11ec138d0ba459c5d25d965357d6bb461296f6
dmhacker <dmhacker@yahoo.com>
2017-11-30 09:32:01 -0800

    Sort files correctly

--- tritonanalytics/__init__.py
+++ tritonanalytics/__init__.py
@@ -16,14 +16,20 @@ logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s',level=log
 logging.info("Logging initialized ... ✓")
 
 # Setup page analytics
-for infile_page in glob.glob(os.path.join(DATA_INFOLDER, '*')):
+infiles = [fn for fn in glob.glob(os.path.join(DATA_INFOLDER, '*')) if 'page' in fn]
+sorted_infiles = sorted(infiles, key=os.path.getctime)
+for infile_page in sorted_infiles:
+
+  # Isolate basename & determine corresponding posts infile
   infilename_page = os.path.basename(infile_page)
-  if 'page' not in infilename_page:
-    continue
   infile_posts = infile_page.replace('page-', 'posts-')
+
+  # Determine outfile basename & determine output file destinations
   outfilename = infilename_page.replace('page-', '').replace('.csv', '.html')
   outfile_page = os.path.join(GRAPHS_OUTFOLDER, 'page', outfilename)
   outfile_posts = os.path.join(GRAPHS_OUTFOLDER, 'posts', outfilename)
+
+  # Generate analytics
   generate_page_analytics(infile_page, infile_posts, outfile_page)
   generate_post_analytics(infile_page, infile_posts, outfile_posts)
 logging.info("Page & post analytics generated ... ✓")
