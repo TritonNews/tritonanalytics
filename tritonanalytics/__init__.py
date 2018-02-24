7d87528cbb2a23bbc5fdb07cce4897f304367d10
David Hacker <dmhacker.cs@gmail.com>
2018-02-24 11:40:33 -0800

    Wait DELAY before first dataframe update (redundant)

--- tritonanalytics/__init__.py
+++ tritonanalytics/__init__.py
@@ -18,33 +18,32 @@ UPDATE_DELAY_SECONDS = 3600 # Every hour
 
 # Setup logging
 logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s',level=logging.INFO)
-logging.info("Logging initialized ... ✓")
+logging.info("Logging initialized.")
 
 # Login to database
 db_uri = os.environ.get("TRITON_ANALYTICS_MONGODB")
 db = MongoClient(db_uri).get_database("tritonanalytics")
-logging.info("Database login successful ... ✓")
+logging.info("Database login successful.")
 
 # Pull information from the database occasionally
 def update_analytics(db):
   while 1:
-    logging.info("Forcing update of dataframes ...")
+    time.sleep(UPDATE_DELAY_SECONDS)
 
+    logging.info("Forcing update of dataframes ...")
     start_time = time.time()
     generate_dataframes(db, force_update=True)
     logging.info("Force update took {0:.2f}s.".format(time.time() - start_time))
 
-    sleep(UPDATE_DELAY_SECONDS)
-
 # Start thread that updates dataframes
 Thread(target=update_analytics, args=(db,)).start()
-logging.info("Database updater started ... ✓")
+logging.info("Dataframe update thread started.)
 
 # Generate analytic HTML files
 generate_page_analytics(db, PAGE_ANALYTICS_OUTFILE)
-logging.info("Page analytics generated ... ✓")
+logging.info("Page analytics generated.")
 generate_post_analytics(db, POST_ANALYTICS_OUTFILE)
-logging.info("Post analytics generated ... ✓")
+logging.info("Post analytics generated.")
 
 # Start the web server
 app = Flask(__name__)
