06066eb5e09ec662df05edd270427e441c55acac
David Hacker <dmhacker.cs@gmail.com>
2018-02-24 11:29:50 -0800

    Add multithreading to update database periodically

--- tritonanalytics/__init__.py
+++ tritonanalytics/__init__.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from pymongo import MongoClient
+from threading import Thread
 
-from tritonanalytics.fbreport import generate_page_analytics, generate_post_analytics
+from tritonanalytics.fbreport import generate_page_analytics, generate_post_analytics, generate_dataframes
 from tritonanalytics.views import main, graphs
 from tritonanalytics.constants import *
 
-from pymongo import MongoClient
-
+import time
 import logging
 import os
 
@@ -20,6 +21,14 @@ logging.info("Logging initialized ... ✓")
 db_uri = os.environ.get("TRITON_ANALYTICS_MONGODB")
 db = MongoClient(db_uri).get_database("tritonanalytics")
 logging.info("Database login successful ... ✓")
+
+# Pull information from the database occasionally
+def update_analytics(db):
+  generate_dataframes(db, force_update=True)
+  sleep(2000) # Little over half an hour
+  update_analytics(db)
+Thread(target=update_analytics, args=(db,))
+logging.info("Database updater started ... ✓")
 
 # Generate analytic HTML files
 generate_page_analytics(db, PAGE_ANALYTICS_OUTFILE)
